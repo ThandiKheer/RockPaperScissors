d00b0d17d6dbe7c5f48ebdc37f4dfc27fc11a41d
AnmolMehta <anmolmehta162@gmail.com>
2023-05-26 16:10:54 +0530

    Create UI for Rock Paper Scissor Game

--- script.js
+++ script.js
@@ -6,24 +6,17 @@ function getComputerChoice(){
         return "paper";
     else
         return "scissors";
-
-}
-function getPlayerChoice(){
-    let index=Math.floor(Math.random() *3 );
-    const choices =[ 'rock', 'paper', 'scissors'];
-    return choices[index];
 }
 
-
-function playRound(playerSelection, computerSelection){
-    if(playerSelection==="rock"){
-        if(computerSelection==="rock"){
+ function PlayRound(computerSelection, playerSelection){
+     if(playerSelection === "rock"){
+        if(computerSelection === "rock"){
             return "Tie";
         }
-        else if(computerSelection==="paper"){
+        else if(computerSelection === "paper"){
              return "Computer Wins";
         }
-        else if(computerSelection==="scissors"){
+        else if(computerSelection === "scissors"){
              return "Player Wins";
         }
     }
@@ -51,27 +44,137 @@ function playRound(playerSelection, computerSelection){
              return "Tie";
         }
     }
+ }
+ 
+ let playerWins = 0;
+ let computerWins = 0;
+ 
+ // -------------RESULT-------------------
+    function resultDisplay(result){
+        const playerResult = document.querySelector('.playerResult');
+        const computerResult = document.querySelector('.computerResult')
+        playerResult.style.fontSize = "30px";
+        computerResult.style.fontSize = "30px";
+        
+        if(result === "Player Wins") {
+            playerWins++;
+            playerResult.textContent = "PLAYER: " +playerWins;
+            computerResult.textContent = "COMPUTER: "+computerWins;
+        }
+        else if(result === "Computer Wins") {
+            computerWins++;   
+            playerResult.textContent = "PLAYER: " +playerWins;
+            computerResult.textContent = "COMPUTER: "+computerWins;
+        }
+        else if(result === "Tie") {
+            playerWins++;
+            computerWins++;
+            playerResult.textContent = "PLAYER: " +playerWins;
+            computerResult.textContent = "COMPUTER: "+computerWins;
+        }
+    
+    
+    if(computerWins === 5 || playerWins === 5){
+        let winner;
+        if(playerWins === 5){
+            playerWins++;
+            playerResult.textContent = "PLAYER: " +playerWins;
+            computerResult.textContent = "COMPUTER: "+computerWins;
+            winner = "PLAYER";
+        }    
+        else if(computerWins === 5){
+            computerWins++;
+            playerResult.textContent = "player: " +playerWins;
+            computerResult.textContent = "computer: "+computerWins;
+            winner = "COMPUTER";
+        }
+        alert(`Winner is ${winner}`);
+        playerWins = 0;
+        computerWins = 0;
+        playerResult.textContent = "player: " + playerWins;
+        computerResult.textContent = "computer: " + computerWins;
+        targetDivPlayer.style.backgroundImage = "url('img/default.jpg.png')";
+        targetDivComputer.textContent = "";
+    }
 }
+    const targetDivComputer =  document.querySelector('.cpuSelection');
+    
 
-let computerWins=0;
-let playerWins=0;
-for(let i=0;i<5;i++){
-    let playerSelection= getPlayerChoice();
-    let computerSelection=getComputerChoice();
-    console.log(`player chose: ${playerSelection} || computer chose: ${computerSelection}`);
-    console.log(playRound( playerSelection, computerSelection ));
-    if(playRound(playerSelection, computerSelection)==="Player Wins"){
-        playerWins++;
+    function displaySelectedComp(computerSelection){
+        const container = document.querySelector('.cpuSelection');
+        container.style.fontSize = "100px";
+        container.style.textAlign = "center";
+        container.style.color = "azure";
+        if(computerSelection === "rock"){
+            container.textContent = " rock";
+        }
+        else if(computerSelection === "paper"){
+            container.textContent = "paper";
+        }
+        else if(computerSelection === "scissors"){
+            container.textContent = " scissors";
+        }
     }
-    else if(playRound(playerSelection, computerSelection)==="Computer Wins"){
-        computerWins++;
-    }
-    else if(playRound(playerSelection, computerSelection)==="Tie") {
-        playerWins++;
-        computerWins++;
-    }
-}
-console.log(playerWins, computerWins);
-playerWins>computerWins ? console.log("Player Wins the game"): console.log("Computer wins the game");
+    
+ //--------------------------------------------------------- 
+ /* NOTE- make the background image the same size of the PlayerSelection div
+             */
+ const targetDivPlayer = document.querySelector('.PlayerSelection');
+ 
+ function handlePressedButton(e){
+    if( this.classList.value == 'RockBtnPlayer'){ 
+        targetDivPlayer.style.backgroundImage = "url('img/playerRock.jpg')";
+        buttonPressed =  'rock';  
+     }
+     else if( this.classList.value == 'PaperBtnPlayer' ){
+        targetDivPlayer.style.backgroundImage = "url('img/paperPlayer.jpg')";
+        buttonPressed = 'paper';
+     }
+     else if( this.classList.value == 'ScissorBtnPlayer' ){
+        targetDivPlayer.style.backgroundImage = "url('img/playerScissorHand.jpg')";
+        targetDivPlayer.style.backgroundSize = "contain";
+        buttonPressed = 'scissors';
+     }
+
+      // COMPUTER SELECTION
+    const computerSelection = getComputerChoice();
+
+/*----------------PLAYROUND FUNCTION CALLED------------------------- - */
+     let result = PlayRound(computerSelection, buttonPressed);
+
+    
+    
+/*-------------FUNCTION TO  SET THE CHOICES OF PLAYER AND COMPUTER TO MAIN CONTAINER-------------------------------- */
+   // displaySelected(buttonPressed);
+    displaySelectedComp(computerSelection);
+
+
+/*-------------------------RESULT DISPLAYED--------------------------------------------- */
+    resultDisplay(result); 
+ }
+
+ 
+ 
+/*------------------------------PLAYER SELECTED BUTTON----------------------------------*/
+ const playerChoiceRock = document.querySelector('.RockBtnPlayer');
+ const playerChoicePaper = document.querySelector('.PaperBtnPlayer');
+ const playerChoiceScissors = document.querySelector('.ScissorBtnPlayer');
+
+ let buttonPressed = null;
+
+ playerChoiceRock.addEventListener('click', handlePressedButton);
+ playerChoicePaper.addEventListener('click', handlePressedButton);
+ playerChoiceScissors.addEventListener('click', handlePressedButton);
+
+
+
+
+
+
+
+
+
+
+
 
 
